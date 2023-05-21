ae7ceb1d0b06b695f5bcf46d45cb662fd0f83a3e
Martin <boidi.martinalejandro@gmail.com>
2023-05-21 19:36:43 -0300

    udpate

--- src/ZonaDeCobertura/ZonaDeCobertura.java
+++ src/ZonaDeCobertura/ZonaDeCobertura.java
@@ -1,5 +1,61 @@
 package ZonaDeCobertura;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Muestra.Muestra;
+
 public class ZonaDeCobertura {
+	
+	
+	private String nombre;
+	private Ubicacion epicentro ;
+	private List<Muestra> muestras;
+	private List<Observadores> observadores;
+	
+	
+	public ZonaDeCobertura(String nombre, Ubicacion epicentro) {
+		super();
+		this.nombre = nombre;
+		this.epicentro = epicentro;
+		this.muestras = new ArrayList<Muestra>();
+		this.observadores = new ArrayList <Observadores>();
+	}
+	
+	
+	
+	
+	public void notificar(String s , Ubicacion u , Muestra m) {
+		
+	}
+	
+	public void agregarObservador(Observador ob) {
+	
+	}
+	
+	public void sacarObservador(Observador ob) {
+		
+	}
+	
+	public void agregarMuestra(Muestra muestra) {
+		
+	}
+	
+	public void  notificaVerificacionMuestra(Muestra muestra) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
