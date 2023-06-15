1245286297bdcdb0a84171e5d582081f6a96f040
Martin <boidi.martinalejandro@gmail.com>
2023-06-15 17:05:40 -0300

    refactor filtro v1

--- src/Filtro/Filtro.java
+++ src/Filtro/Filtro.java
@@ -6,17 +6,4 @@ import Muestra.Muestra;
 public abstract class Filtro {
 
 	public abstract List<Muestra> buscar(List<Muestra> muestras);
-	
-	public void agregarFiltro(Filtro filtro) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public void quitarFiltro(Filtro filtro) {
-		throw new UnsupportedOperationException();
-	}
-
-	public Filtro getFiltroHijo(int index) {
-		throw new UnsupportedOperationException();
-	}
-	
 }
